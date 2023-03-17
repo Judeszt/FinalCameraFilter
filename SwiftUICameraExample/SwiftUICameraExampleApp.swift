a4fd774f42faf87a62352d2bea9e50fff0cc372b
Judeszt <126808542+Judeszt@users.noreply.github.com>
2023-03-17 13:24:04 -0700

    initial commit

---
+++ SwiftUICameraExample/SwiftUICameraExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICameraExampleApp.swift
+//  SwiftUICameraExample
+//
+//  Created by Jude Sztraicher on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICameraExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
